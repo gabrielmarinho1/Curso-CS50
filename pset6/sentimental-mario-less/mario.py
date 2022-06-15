db46baf878aa90534ef43c7b4cdef8478b175e0c
gabrielmarinho1 <gabriel.marinho112@gmail.com>
2022-06-15 17:59:25 -0300

    sentimental_mario-less

---
+++ pset6/sentimental-mario-less/mario.py
@@ -0,0 +1,14 @@
+# TODO
+
+from cs50 import get_int
+
+# get height from 1 to 8
+while True:
+    height = get_int("Height: ")
+    if height > 0 and height <= 8:
+        break
+
+# print pyramid
+for hash in range(1, height + 1):
+    print(" " * int(height - hash), end="")
+    print("#" * hash)
